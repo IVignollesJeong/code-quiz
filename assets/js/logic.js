0f78eff5e8b49fea50ad4236418d0bbabc753a16
Ian Vignolles-Jeong <ianvignolles@gmail.com>
2023-05-26 21:24:01 -0400

    added questions, added start button functionality/first question, combined questions.js and logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,26 +1,70 @@
 // variables to keep track of quiz state
 var currentQuestionIndex = 0;
 //time left value here
-// var time = ;
+var time = 60;
 var timerId;
 
+
+// variable to store our questions
+
+var questions = [
+  {
+    title: 'DOM stands for:',
+    choices: ['Department of Motorvehicles', 'Document Oriented Management', 'Document Object Model', 'Div Object Manipulation'],
+    answer: 'Document Object Model',
+  },
+
+  {
+    title: 'JSON stands for:?',
+    choices: ['JavaScript Overlay, New', 'JavaScript Object Notation', 'Java Saving Oriented Notation', 'Jump Script Operating Network'],
+    answer: 'JavaScript Object Notation',
+  },
+
+  {
+    title: 'String values must be enclosed within _____ when being assigned to variables.',
+    choices: ['parentheses', 'quotes', 'curly brackets', 'element tags <>'],
+    answer: 'quotes',
+  },
+
+  {
+    title: 'Which operator evaluates if one OR the other expression is true?',
+    choices: ['&&', '!==', '=+', '||'],
+    answer: '||',
+  },
+
+  {
+    title: 'what is the index of Apple in the following array? Apple, Banana, Pear, Strawberry',
+    choices: ['1', 'Apple', '0', 'First'],
+    answer: '0',
+  },
+
+  {
+    title: 'How can we typically manipulate HTML by using JavaScript?',
+    choices: ['Operators', 'Console.log', 'GitHub', 'JSON'],
+    answer: 'JSON',
+  },
+];
+
 // variables to reference DOM elements
-var questionsEl = document.getElementById('');
-var timerEl = document.getElementById('');
-var choicesEl = document.getElementById('');
-var submitBtn = document.getElementById('');
-var startBtn = document.getElementById('');
-var initialsEl = document.getElementById('');
-var feedbackEl = document.getElementById('');
+var questionsEl = document.getElementById('questions');
+var timerEl = document.getElementById('time');
+var choicesEl = document.getElementById('choices');
+var submitBtn = document.getElementById('submit');
+var startBtn = document.getElementById('start');
+var initialsEl = document.getElementById('initials');
+var feedbackEl = document.getElementById('feedback');
+
+var sfxRight = new Audio('assets/sfx/correct.wav');
+var sfxWrong = new Audio('assets/sfx/incorrect.wav');
 
 
 function startQuiz() {
   // hide start screen
-  var startScreenEl = document.getElementById('');
-  startScreenEl.setAttribute('class', '');
+  var startScreenEl = document.getElementById('start-screen');
+  startScreenEl.setAttribute('class', 'hide');
 
   // un-hide questions section
-  questionsEl.removeAttribute('');
+  questionsEl.removeAttribute('class');
 
   // start timer
   timerId = setInterval(clockTick, 1000);
@@ -36,83 +80,48 @@ function getQuestion() {
   var currentQuestion = questions[currentQuestionIndex];
 
   // update title with current question
-  var titleEl = document.getElementById('');
-  titleEl.textContent = ; //think dot notation
+  var titleEl = document.getElementById('question-title');
+  titleEl.textContent = currentQuestion.title; //think dot notation
 
   // clear out any old question choices
   choicesEl.innerHTML = '';
 
   // loop over choices
-  for (var i = 0; i < ; i++) {
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
     // create new button for each choice
     var choice = currentQuestion.choices[i];
-    var choiceNode = document.createElement('');
+    var choiceNode = document.createElement('button');
     choiceNode.setAttribute('class', 'choice');
     choiceNode.setAttribute('value', choice);
 
     choiceNode.textContent = i + 1 + '. ' + choice;
 
     // display on the page
-    choicesEl.appendChild();
+    choicesEl.appendChild(choiceNode);
   }
 }
 
-function questionClick(event) {
-  var buttonEl = event.target;
-
-  // if the clicked element is not a choice button, do nothing.
-  if (!buttonEl.matches('.choice')) {
-    return;
-  }
-
-  // check if user guessed wrong
-  if () {
-
-
-
-    // penalize time
-    
-  
-   
-
-    // display new time on page
-   
-
-  // flash right/wrong feedback on page for half a second
- 
-
-  // move to next question
-  
-
-  // check if we've run out of questions or if time ran out?
-  if () {
-
-    //if it did ???
-
-  } else {
-    
-    // if it didnt??
-  }
-}
 
 function quizEnd() {
   // stop timer
- 
+ clearInterval(timerId);
   // show end screen
-  var endScreenEl = document.getElementById('');
+  var endScreenEl = document.getElementById('end-screen');
   endScreenEl.removeAttribute('class');
 
   // show final score
-  var finalScoreEl = document.getElementById('');
+  var finalScoreEl = document.getElementById('final-score');
   finalScoreEl.textContent = time;
 
   // hide questions section
+  questionsEl.setAttribute('class', 'hide');
 }
 
 function clockTick() {
   // update time
   // decrement the variable we are using to track time
-  timerEl.textContent = ; // update out time
+  time--;
+  timerEl.textContent = time; // update out time
 
   // check if user ran out of time
   if (time <= 0) {
@@ -125,12 +134,12 @@ function saveHighscore() {
   var initials = initialsEl.value.trim();
 
   // make sure value wasn't empty
-  if () {
+  if (initials !== '') {
 
     // get saved scores from localstorage, or if not any, set to empty array
     
     var highscores =
-      JSON.parse() /* what would go inside the PARSE??*/ || [];
+      JSON.parse(window.localStorage.getItem('highscores')) /* what would go inside the PARSE??*/ || [];
 
     // format new score object for current user
     var newScore = {
@@ -140,10 +149,10 @@ function saveHighscore() {
 
     // save to localstorage
     highscores.push(newScore);
-    window.localStorage.setItem('highscores', JSON.stringify(/* What would we put inside STRINGIFY? */));
+    window.localStorage.setItem('highscores', JSON.stringify(highscores));
 
     // redirect to next page
-    window.location.href = '';
+    window.location.href = 'highscores.html';
   }
 }
 
